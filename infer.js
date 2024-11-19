271aadcc7cc67915c4dcbed3327c503149ed30d7
Moattar Ansari <moattaransari62@gmail.com>
2024-11-19 07:55:12 +0500

    Add files via upload

---
+++ infer.js
@@ -0,0 +1,5 @@
+"use strict";
+//-----------------------------------Type Inference-------------------------------------- 
+let firstName = "Moattar"; // The complier infers the type string for the variable name.
+let age = 17; // The compiler infers the type number for variable age.
+let isValid = true; //The compiler infers the type boolean the variable is valid
